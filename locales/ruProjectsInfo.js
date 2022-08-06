34633ab89e7dcef724d563278439d518cd48563e
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-08-06 11:18:28 +0400

    new updates

---
+++ locales/ruProjectsInfo.js
@@ -0,0 +1,27 @@
+export default {
+  cur: "Текущий",
+  name: "OLD CITY PANORAMA",
+  loc: "Локация",
+  loc1: "ул. Михеила Бухаидзе №48",
+  time: "Время сдачи",
+  time2: "2023 год",
+  //line break
+  name: "EZO OLD TBILISI",
+  loc: "Локация",
+  loc1: "Михеила Бухаидзе №48",
+  time: "Время сдачи",
+  time2: "2024 год",
+  //line break
+  name: "TAGHI OLD TBILISI",
+  loc: "Локация",
+  loc1: "ул. Михеила Бухаидзе №48",
+  time: "Время сдачи",
+  time2: "2024 год",
+  //line break
+  cur: "Завершенный",
+  loc: "Локация",
+  loc1: "ул. Михеила Бухаидзе,  I-ый переулок, 6а",
+  time: "Время сдачи",
+  time2: "2022 год",
+  //line break
+};
