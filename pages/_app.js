98a79d2429b6e0b3aa9c1a24dc7c1e0d698d0439
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-08-06 12:12:09 +0400

    index page creation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/index.css'
-
+import '../styles/applications.scss'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Nav from '../components/nav';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-
+import { Row, Col,Container,Form,Button } from "react-bootstrap";
 import en from '../locales/en';
 import fr from '../locales/fr';
-import ViewSource from '../components/github';
+import Slide1 from "../assets/slider.png";
+import Taghi from "../assets/taghi.svg"
+import Apart_showroom from "../assets/apartshowroom.svg";
+
+import Malongo from "../assets/malongo2.png";
+import Tbc from "../assets/tbc-logo-geo2.svg";
 
 export default function IndexPage() {
   const [showBanner, setBanner] = useState(true);
@@ -15,20 +20,186 @@ export default function IndexPage() {
 
   return (
     <main>
-      <ViewSource />
       <div className="hero">
         <div className="bg-image">
           <span className="bg-black opacity-40 absolute w-full h-full z-10" />
-          <Image
-            alt="Hero"
-            src="/hero.jpg"
-            layout="fill"
-            objectFit="cover"
-            priority
-          />
         </div>
         <Nav />
-        <section className="pt-8 pb-20 flex flex-col items-center">
+              <Container>
+                <Row>
+                  <Col  lg="6" md="6" sm="6" xl="6" xxl="6" >
+                    <Image src={Slide1} />
+                  </Col>
+                  <Col  lg="6" md="6" sm="6" xl="6" xxl="6" className="d-flex">
+                    <Col className="d-flex align-items-center">
+                      <div className="test">
+                        <Image src={Taghi} alt="banner immage" className="Taghi" />
+                        <p>
+                          For our team, it is important and a priority to preserve <br />
+                          the charm of the old city, to use the old
+                          <br />
+                          <br />
+                          For our team, it is important and a priority to preserve <br /> the charm of the old
+                          city, to use the old
+                        </p>
+                      </div>
+                    </Col>
+                  </Col>
+                </Row>
+              </Container>
+              <Container className="container-marginer additional__padding">
+      <Row className="justify-content-between">
+        <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
+          <Row>
+            <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3">
+              <p className="home--section__texts">About us</p>
+            </Col>
+            <Col></Col>
+            <Col xs="12" lg="8" md="8" sm="8" xl="8" xxl="8">
+              <h1 className="section1-title">
+                RENEWED LIFE
+                <br /> OF OLD TBILISI
+              </h1>
+
+              <p className="section1-subtitle">
+                For our team, it is important and a priority to preserve the charm of the old city, to use the
+                old architectural fragments in the modern construction, and at the same time to bring new life
+                to the forgotten neighborhoods.
+              </p>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs="12" lg="5" md="5" sm="5" xl="5" xxl="5">
+          <Col xs="12" className="col-5 padding-0">
+            <p className="section1-form-text">
+              Leave us your contact information and our team will reach out to you.
+            </p>
+            <h4 className="section-1-form-subtext">Apartment type:</h4>
+            <Row>
+              <Col xs="3">
+                <Form>
+                  {["checkbox"].map(type => (
+                    <div key={`inline-${type}`} className="mb-3">
+                      <Form.Check inline label="Studio" name="group1" type={type} id={`inline-${type}-1`} />
+                    </div>
+                  ))}
+                </Form>
+              </Col>
+              <Col xs="3">
+                <Form>
+                  {["checkbox"].map(type => (
+                    <div key={`inline-${type}`} className="mb-3 home--inputs">
+                      <Form.Check
+                        inline
+                        label="1 Bedroom"
+                        name="group1"
+                        type={type}
+                        id={`inline-${type}-2`}
+                      />
+                    </div>
+                  ))}
+                </Form>
+              </Col>
+              <Col xs="6" className="col-6">
+                <Form>
+                  {["checkbox"].map(type => (
+                    <div key={`inline-${type}`} className="mb-3 home--inputs">
+                      <Form.Check
+                        inline
+                        label="2+ Bedrooms"
+                        name="group1"
+                        type={type}
+                        id={`inline-${type}-3`}
+                      />
+                    </div>
+                  ))}
+                </Form>
+              </Col>
+              <Col className="mb-60">
+                <div xs="12" className="mb-md-0 mb-5 home--inputs">
+                  <Form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                    <Col xs="12">
+                      <div className="md-form mb-0">
+                        <Form.Control
+                          type="text"
+                          id="name"
+                          name="name"
+                          className="form-control input__main"
+                          placeholder="Name"
+                        />
+                      </div>
+                    </Col>
+                    <Col xs="12">
+                      <div className="md-form mb-0">
+                        <Form.Control
+                          type="text"
+                          id="email"
+                          name="email"
+                          className="form-control input__main"
+                          placeholder="Email"
+                        />
+                      </div>
+                    </Col>
+                  </Form>
+                  <div className="text-center text-md-left home--get-contacted">
+                    <Button className="btn-send">Get contacted</Button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Col>
+      </Row>
+    </Container>
+    <Container className="partners-container mb-60 mt-60">
+      <Row className="justify-content-between align-items-center ">
+        <Col xs="12" lg="2" md="2" sm="2" xl="2" xxl="2">
+          <div className="partners home--section__texts">
+            <p className="home--section__texts">Partners</p>
+          </div>
+        </Col>
+        <Col xs="12" lg="10" md="10" sm="10" xl="10" xxl="10">
+          <Row className="justify-content-between">
+            <div className="col-3 ">
+              <div className="partners">
+                <Image src={Apart_showroom} alt="banner immage" className="partners_wite" />
+              </div>
+            </div>
+            {/* <div className="col-2">
+          <div className="partners">
+            <Image src={Greenlife} alt="banner immage" className="partners_wite" />
+          </div>
+        </div> */}
+            <div className="col-3">
+              <div className="partners">
+                <Image src={Malongo} alt="banner immage" className="partners_wite" />
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="partners">
+                <Image src={Tbc} alt="banner immage" className="partners_wite" />
+              </div>
+            </div>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+    <Container className="mb-60 mt-60">
+        <Row>
+          <Col xs="12" lg="2" md="2" sm="2" xl="2" xxl="2">
+            <p className="home--section__texts">Projects Map</p>
+          </Col>
+          <Col xs="12" lg="10" md="10" sm="10" xl="10" xxl="10">
+            <iframe
+              src="https://snazzymaps.com/embed/408105"
+              width="100%"
+              height="600px"
+              // style="border:none;"
+            ></iframe>
+          </Col>
+        </Row>
+      </Container>
+        {/* <section className="pt-8 pb-20 flex flex-col items-center">
           <nav aria-label="breadcrumb">
             <ol className="flex space-x-2 text-white text-sm mb-8 text-shadow">
               <li>
@@ -45,9 +216,9 @@ export default function IndexPage() {
           </h1>
           <hr className="mt-16 mb-8 w-32 border-t-2 border-white" />
           <p className="text-white text-shadow px-8">{t.description}</p>
-        </section>
+        </section> */}
       </div>
-      <article className="mt-16 px-8 max-w-4xl m-auto">
+      {/* <article className="mt-16 px-8 max-w-4xl m-auto">
         <p className="mb-8">{t.p1}</p>
         <Image
           alt="Screenshot"
@@ -57,7 +228,7 @@ export default function IndexPage() {
         />
         <p className="my-8">{t.p2}</p>
         <p className="mb-16">{t.p3}</p>
-      </article>
+      </article> */}
       {showBanner && (
         <div className="flex flex-col items-center pt-4 bg-gray-800 text-white fixed bottom-0 w-full bg-opacity-80 p-4 text-center">
           <p>{t.cookies}</p>
