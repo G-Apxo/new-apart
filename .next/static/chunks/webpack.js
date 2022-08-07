eb1860de04e1efde6e7aff42986937effcc01673
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-08-07 19:24:15 +0400

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ecc14243277d872"; }
+/******/ 		__webpack_require__.h = function() { return "d0ce42edf96b2b03"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
